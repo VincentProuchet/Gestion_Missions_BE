3aac25a8dca1f46206a432a412c75c5f588452d9
Joseph MECHALI <mechali.joseph@gmail.com>
2022-09-07 10:45:06 +0200

    first commit : spring project with spring web, spring data jpa, mysql driver, lombock, liquibase

---
+++ src/main/java/diginamic/gestion_des_missions_BE/GestionDesMissionsBeApplication.java
@@ -0,0 +1,13 @@
+package diginamic.gestion_des_missions_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDesMissionsBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDesMissionsBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/diginamic/gestion_des_missions_BE/GestionDesMissionsBeApplicationTests.java
@@ -0,0 +1,13 @@
+package diginamic.gestion_des_missions_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDesMissionsBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
