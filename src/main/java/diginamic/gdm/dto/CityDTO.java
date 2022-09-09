e8d4964fba4fd4dd9321cdf9b033a8cdb217503b
Vincent Prouchet <prouchetvincent@hotmail.com>
2022-09-09 15:47:05 +0200

    CityDTO

---
+++ src/main/java/diginamic/gdm/dto/CityDTO.java
@@ -0,0 +1,24 @@
+package diginamic.gdm.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * 
+ * @author Vincent
+ *
+ */
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class CityDTO {
+	
+	/** id */
+	private int id;
+
+	/** name : the name of the city */
+	private String name;
+}
