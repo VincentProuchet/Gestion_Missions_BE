38b37a1ac7b972ac7290819f2be3972419cfc215
Joseph MECHALI <mechali.joseph@gmail.com>
2022-09-22 17:21:36 +0200

    first fix allow cors

--- src/main/java/diginamic/gdm/GestionDesMissionsBeApplication.java
+++ src/main/java/diginamic/gdm/GestionDesMissionsBeApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @SpringBootApplication
@@ -14,6 +17,16 @@ public class GestionDesMissionsBeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GestionDesMissionsBeApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/login").allowedOrigins("http://localhost:4200");
+			}
+		};
 	}
 
 }
